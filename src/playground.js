bda77ff1c11e4a2fc5d37dca1215015bbd6489cf
jonathan coronel <jcoronel94@gmail.com>
2020-10-14 19:20:48 -0400

    init

---
+++ src/playground.js
@@ -0,0 +1,14 @@
+export default function(){
+    console.log("hello world");
+
+    // var firstName = "flip";
+    // var age =28;
+    // var isMale = true;
+
+
+    function print(){
+        console.log("log user")
+    }
+
+    print()
+}
